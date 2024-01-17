a9ba80731fdde4c15e4312344a1c3bf15bf7bf5b
Josh Bakelaar <josh.a.bakelaar@gmail.com>
2024-01-16 22:49:57 -0500

    Fixed Hero Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,31 @@ function isMobile() {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-// Dynamically create script element and set its attributes
-var scriptElement = document.createElement('script');
-scriptElement.type = 'module';
-scriptElement.src = 'https://unpkg.com/@splinetool/viewer@1.0.29/build/spline-viewer.js';
+// Wait for the DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Dynamically create script element and set its attributes
+  var scriptElement = document.createElement('script');
+  scriptElement.type = 'module';
+  scriptElement.src = 'https://unpkg.com/@splinetool/viewer@1.0.29/build/spline-viewer.js';
 
-// Dynamically create spline-viewer element and set its attributes
-var splineViewerElement = document.createElement('spline-viewer');
-splineViewerElement.setAttribute('loading-anim-type', 'none');
-splineViewerElement.setAttribute('url', 'https://prod.spline.design/dAeh4EN-fQTpL2b4/scene.splinecode');
+  // Dynamically create spline-viewer element and set its attributes
+  var splineViewerElement = document.createElement('spline-viewer');
+  splineViewerElement.setAttribute('loading-anim-type', 'none');
+  splineViewerElement.setAttribute('url', 'https://prod.spline.design/dAeh4EN-fQTpL2b4/scene.splinecode');
 
-// Check if the device is a phone and append the script and spline-viewer elements accordingly
-if (isMobile()) {
-  document.head.appendChild(scriptElement);
-  document.body.appendChild(splineViewerElement);
-} else {
-  // If it's not a phone, you can add additional logic or elements here
-  document.head.appendChild(scriptElement);
-  document.body.appendChild(splineViewerElement);
-}
+  // Check if the device is a phone and append the script and spline-viewer elements to .hero
+  if (isMobile()) {
+    var heroElement = document.querySelector('.hero');
+    if (heroElement) {
+      heroElement.appendChild(scriptElement);
+      heroElement.appendChild(splineViewerElement);
+    } else {
+      // Handle the case where .hero is not found
+      console.error('.hero element not found.');
+    }
+  } else {
+    // If it's not a phone, you can add additional logic or elements here
+    document.body.appendChild(scriptElement);
+    document.body.appendChild(splineViewerElement);
+  }
+});
